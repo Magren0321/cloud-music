68f707310c282c6ae03871893ba2ba3d9505aa25
Magren <44224328+Magren0321@users.noreply.github.com>
2021-01-03 22:58:08 +0800

    歌单详情页

--- src/views/songListInfo/songListInfo.vue
+++ src/views/songListInfo/songListInfo.vue
@@ -2,11 +2,18 @@
   <div>
       <div>
         <div class="tab">
-          <v-icon large @click="returnPage()">mdi-arrow-left</v-icon>
+          <v-icon large color="#fff" @click="returnPage()">mdi-arrow-left</v-icon>
           <p>{{title}}</p>
         </div>
+        <div class="songlist_info">
+          <img :src="songImg">
+          <div class="songlistText">
+            <p id="listName">{{songListName}}</p>
+            <p id="creatorName">{{songListCreator}}</p>
+          </div>
+        </div>
       </div>
-      <div>
+      <div class="songlist">
         
       </div>
   </div>
@@ -14,6 +21,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import api from '@/api/index'
 
 @Component({
   components: {
@@ -21,14 +29,20 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class SongListInfo extends Vue {
-  title = "歌单"
+  title = "歌单";
+  songImg = require('@/assets/like.png');
+  songListName = "歌单名称";
+  songListCreator = "创建者";
+  songlist: any[] = []; //歌单
 
-  returnPage(){
+  returnPage(): void{
     this.$router.go(-1);
   }
   
   mounted () {
-    console.log(this.$route.params.id)
+    api.getSonglistInfo(this.$route.params.id).then((res: object|any)=>{
+      console.log(res);
+    });
   }
 }
 </script>
@@ -36,6 +50,7 @@ export default class SongListInfo extends Vue {
 <style scoped>
 .tab{
   display: flex;
+  background-color: #F3350C;
   height: 40px;
 }
 .tab p{
@@ -43,5 +58,28 @@ export default class SongListInfo extends Vue {
   margin-left: 10px;
   font-weight: bold;
   font-size: 21px;
+  color: #fff;
+}
+.songlist_info{
+  display: flex;
+  align-items: center;     
+  background-color:#F3350C;
+  height: 200px;
+}
+.songlist_info img{
+  width: 130px;
+  height: 130px;
+  margin-left: 35px;
+}
+.songlistText{
+  margin-left: 20px;
+}
+#listName{
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+}
+#creatorName{
+  color: #fff;
 }
 </style>
